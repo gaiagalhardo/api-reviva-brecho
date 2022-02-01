22bfd87bb65c279a77f37db2df957ef0655f94cd
Gaia Galhardo <ti.gaiagalhardo@gmail.com>
2022-02-01 18:24:22 -0300

    feat: add project api-reviva

---
+++ src/main/java/com/gaiagalhardo/reviva/ApiRevivaBrechoApplication.java
@@ -0,0 +1,13 @@
+package com.gaiagalhardo.reviva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRevivaBrechoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRevivaBrechoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gaiagalhardo/reviva/ApiRevivaBrechoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaiagalhardo.reviva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRevivaBrechoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
